5a7e5a653a38efb9d9441562e5ff5c7c22274d5a
CharlesGarrocho <charles.garrocho@gmail.com>
2013-05-09 14:22:51 -0300

    adicionado: novo rótulo e configuração das abas.

--- source/src/com/mycurrentip/MyCurrentIP.java
+++ source/src/com/mycurrentip/MyCurrentIP.java
@@ -1,22 +1,32 @@
 package com.mycurrentip;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 public class MyCurrentIP extends Activity {
+
+	private TabHost tabHost;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_my_current_ip);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.my_current_i, menu);
-		return true;
-	}
+		tabHost = (TabHost)findViewById(R.id.activity_my_current_ip_tab_host);
+		tabHost.setup();
 
+		TabSpec spec1 = tabHost.newTabSpec("IP Atual");
+		TabSpec spec2 = tabHost.newTabSpec("IPs Anteriores");
+
+		spec1.setIndicator("IP Atual");
+		spec2.setIndicator("IPs Anteriores");
+
+		spec1.setContent(R.id.aba_ip_atual);
+		spec2.setContent(R.id.aba_ips_anteriores);
+
+		tabHost.addTab(spec1);
+		tabHost.addTab(spec2);
+	}
 }
