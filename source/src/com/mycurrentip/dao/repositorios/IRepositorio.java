a01bb5346bc6e2eafafb2b4b8e5fb3ecdc896dea
CharlesGarrocho <charles.garrocho@gmail.com>
2013-05-17 09:49:21 -0300

    adicionado: interface dos repositórios.

---
+++ source/src/com/mycurrentip/dao/repositorios/IRepositorio.java
@@ -0,0 +1,19 @@
+package com.mycurrentip.dao.repositorios;
+
+import java.util.List;
+import java.util.Map;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+
+
+public interface IRepositorio<T> {
+	public ContentValues createContentValues(T object);
+	public long insert(T object);
+	public int update(T object);
+	public Cursor getCursor();
+	public Map<String, Integer> getIndices(Cursor cursor);
+	public T createObject(Cursor cursor);
+	public List<T> listar();
+	public void fecharDB();
+}
