ed093037c3d077a24eda50d1dcad63e7aec05a9f
CharlesGarrocho <charles.garrocho@gmail.com>
2013-05-17 11:23:09 -0300

    corrigido: inserção de data hora agora manual.

--- source/src/com/mycurrentip/dao/repositorios/RepositorioHistorico.java
+++ source/src/com/mycurrentip/dao/repositorios/RepositorioHistorico.java
@@ -32,6 +32,7 @@ public class RepositorioHistorico implements IRepositorio<Historico>{
 	public ContentValues createContentValues(Historico Historico){
 		ContentValues valores = new ContentValues();
 		valores.put(Historicos.IP, Historico.getIp());
+		valores.put(Historicos.DATA_HORA, Historico.getData_hora().toString());
 		return valores;
 	}
 
