8f7220586ce7d550552f19bd7e905011990c8b3c
paulovf <paulovfrancisco@gmail.com>
2013-05-29 08:54:09 -0300

    alterado: nome da classe tarefaatualizaip para tarefaatualizainfo #1

--- source/src/com/mycurrentip/net/ClienteHttp.java
+++ source/src/com/mycurrentip/net/ClienteHttp.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.Type;
-import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,10 +25,6 @@ import android.util.Log;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
 
 public class ClienteHttp {
 
